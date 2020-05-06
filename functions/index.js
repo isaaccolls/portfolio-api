1f8637a83bcb541a575e8ec9f64246505887e5ad
Isaac Colls <colls_isaac@hotmail.com>
2020-05-06 16:22:58 -0400

    get everything

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const config = require('./firebase-config.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+firebase.initializeApp({
+  credential: firebase.credential.cert(config),
+  databaseURL: 'https://portfolio-api-77f4e.firebaseio.com/',
+});
+
+exports.api = functions.https.onRequest((req, res) => {
+  res.header('Content-Type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'GET') {
+    const data = firebase.database().ref('/');
+    data.on('value', (snapshot) => {
+      // res.json(snapshot.val());
+      res.json({ test: 420 });
+    });
+  }
+});
